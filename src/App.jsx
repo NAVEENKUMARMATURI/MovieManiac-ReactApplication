266918b2fc46b70184730dcc2c26ad50b3f70377
NAVEENKUMARMATURI <nmz28@mst.edu>
2024-03-26 00:28:13 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import "./App.css";
+import Fire from "./assets/fire.png";
+import Star from "./assets/glowing-star.png";
+import Party from "./assets/partying-face.png";
+import Navbar from "./components/Navbar/Navbar";
+import MovieList from "./components/MovieList/MovieList";
+import { Route, Routes } from "react-router-dom";
+import SingleMovie from "./components/MovieList/SingleMovie";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={<MovieList type="popular" title="Popular" emoji={Fire} />}
+          />
+          <Route
+            path="/top_rated"
+            element={
+              <MovieList type="top_rated" title="Top Rated" emoji={Star} />
+            }
+          />
+          <Route
+            path="/upcoming"
+            element={
+              <MovieList type="upcoming" title="Upcoming" emoji={Party} />
+            }
+          />
+          <Route path="/movie/:movieId" element={<SingleMovie />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default App;
